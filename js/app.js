319dbb446f3594c48d4a450c02eb7c7bef6e1af9
jojohve <joseph.laluna@outlook.it>
2024-02-29 12:11:35 +0100

    fine progetto

--- js/app.js
+++ js/app.js
@@ -1,20 +1,61 @@
 let contatore = 0;
 let buttonPiu = document.getElementById("incrementatore");
 let buttonMeno = document.getElementById("decrementatore");
-let counter = document.getElementById("output")
+let counter = document.getElementById("output");
+const pecora1 = document.querySelector(".closed")
+const pecora2 = document.querySelector(".open")
+const pecora3 = document.querySelector(".open2")
 
 risultato();
+ciao();
 
 buttonPiu.addEventListener("click", () => {
+    if (pecora1.classList.contains(`closed`)) {
+        pecora1.classList.remove(`active`);
+        pecora2.classList.remove(`active`);
+        pecora3.classList.add(`active`)
+    }
     contatore++
     risultato();
+    cinquanta();
+    cento();
 });
 
 buttonMeno.addEventListener("click", () => {
+    if (pecora1.classList.contains(`closed`)) {
+        pecora1.classList.remove(`active`);
+        pecora3.classList.remove(`active`);
+        pecora2.classList.add(`active`)
+    }
     contatore--
     risultato();
+    warning();
 });
 
 function risultato() {
     counter.innerHTML = contatore;
 }
+
+function cinquanta() {
+    if (contatore == 50) {
+        alert("Sei un osso duro, perchè non provi con una tisana?");
+    }
+}
+
+function cento() {
+    if (contatore == 100) {
+        alert("Non riesci proprio ad addormentarti, perchè non provi con la melatonina?");
+    }
+}
+
+function ciao() {
+    document.addEventListener("DOMContentLoaded", () => {
+        alert("Ciao, prova a contare le pecore con me per prendere sonno più velocemente!");
+    });
+}
+
+function warning() {
+    if (contatore == -10) {
+        alert("La pecora sta perdendo la pazienza, così farai fatica ad addormentarti!");
+    }
+  }
